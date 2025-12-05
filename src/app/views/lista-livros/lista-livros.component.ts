0fb2db1284bac38bb019d01955c3feaab5b8c6fa
Guilherme Torette <guiitorette@gmail.com>
2025-12-05 12:27:54 -0300

    projeto pronto

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,59 @@
+import { Component, } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, switchMap, tap, filter, debounceTime, distinctUntilChanged, catchError, throwError, of } from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  campoBusca = new FormControl()
+  mensagemErro = ''
+  livrosResultado: LivrosResultado;
+
+  constructor(private service: LivroService) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro => {
+      console.log(erro)
+      return of()
+    })
+  )
+
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => resultado.items ?? []),
+    map((items) => this.livroResultadoParaLivros(items)),
+    catchError(erro => {
+      console.log(erro)
+      return throwError(()=> new Error(this.mensagemErro = 'ops, ocorreu um erro, Recarregue a aplicação!'))
+    })
+  );
+
+  livroResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+
+}
+
+
+
